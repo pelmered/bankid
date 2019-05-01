aab30230201901fd85e44b4f8e1b81c8cce3426c
Peter Elmered <peter@elmered.com>
2019-05-01 10:26:32 +0200

    Add basic service provider

---
+++ src/BankIDServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Modules\BankID;
+
+use Illuminate\Support\ServiceProvider;
+
+class RiakServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(BankID::class, function () {
+            return new BankID();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/bankid.php' => config_path('bankid.php'),
+        ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+
+        $this->loadTranslationsFrom(__DIR__.'/lang', 'bankid');
+
+        $this->publishes([
+            __DIR__.'/lang' => resource_path('lang/vendor/bankid'),
+        ]);
+    }
+}
